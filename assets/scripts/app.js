2bb0315d953a7a46dd9ae36fec53374f9d235968
Yurii Soroka <urasor04@gmail.com>
2024-08-14 15:00:03 +0300

    Added new class, split render logic between two classes provided better code structuring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,4 @@
 class Product {
-
   constructor(title, image, desc, price) {
     this.title = title;
     this.imageUrl = image;
@@ -8,8 +7,31 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+              <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                <div class = "product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+                </div>
+              </div>
+              `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A Pillow",
       "https://cdn.thewirecutter.com/wp-content/media/2023/01/bedpillows-2048px-9987.jpg?auto=webp&quality=75&width=1024",
@@ -17,34 +39,26 @@ const productList = {
       19.99
     ),
     new Product(
-        "A Carpet",
-        "https://m.media-amazon.com/images/I/81GyZXnRB5L._AC_UF894,1000_QL80_.jpg",
-        "A carpet which you might like or not.",
-        89.99
-      ),
-  ],
+      "A Carpet",
+      "https://m.media-amazon.com/images/I/81GyZXnRB5L._AC_UF894,1000_QL80_.jpg",
+      "A carpet which you might like or not.",
+      89.99
+    ),
+  ];
+
+  constructor() {}
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-            <div>
-              <img src="${prod.imageUrl}" alt="${prod.title}">
-              <div class = "product-item__content">
-              <h2>${prod.title}</h2>
-              <h3>\$${prod.price}</h3>
-              <p>${prod.description}</p>
-              <button>Add to Cart</button>
-              </div>
-            </div>
-            `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
